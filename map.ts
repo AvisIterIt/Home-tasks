15f54c9f9f537e7faf08f1cd71bfaef6dee17691
artur <example@email.com>
2024-11-04 21:56:24 +0500

    todolist dom

--- map.ts
+++ map.ts
@@ -1,7 +1,7 @@
-export {}
+export {};
 
-const arr = ["Aaaa", "Bbbbbb", "Cc"]
+const arr = ["Aaaa", "Bbbbbb", "Cc"];
 
-const newArray = arr.map((x) => x.length > 3) // [4, 6, 2]
+const newArray = arr.map((x) => x.length > 3); // [4, 6, 2]
 
-console.log(newArray)
+console.log(newArray);
